7cc50c955d3fd09a3513312f29b853c5e4b0c96a
Gabriel Fadraga Abreu <fadragagabriel@gmail.com>
2024-03-26 15:23:54 -0400

    Ej1PE

---
+++ counter.vhd
@@ -0,0 +1,12 @@
+
+Started : "Check Syntax for counter".
+Running xst...
+Command Line: xst -intstyle ise -ifn {/home/ise/Gabriel/Desktop/Tareas TAdE/Pregunta1Preparacion/counter.xst} -ofn counter.stx
+
+=========================================================================
+*                          HDL Compilation                              *
+=========================================================================
+Compiling vhdl file "/home/ise/Gabriel/Desktop/Tareas TAdE/Pregunta1Preparacion/ej1.vhd" in Library work.
+Architecture behavioral of Entity counter is up to date.
+
+Process "Check Syntax" completed successfully
